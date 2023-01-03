52ee969b50c8995c795c97e9251445ef09164d95
tingzheng94 <tingzheng94@gmail.com>
2023-01-03 14:04:46 +0800

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ let testaaaaa = 10;
 let test = 2;
 
 let helloworld = 0;
+console.log("aoishdihasidhasd");
 
+console.log("aoishdihasidhasd");
 console.log("aoishdihasidhasd");
 console.log(test, helloworld);
